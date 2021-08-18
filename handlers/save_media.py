f97f6af23d7fb18caff068f952e4b49c05190caa
gplkavin <81810902+gplkavin@users.noreply.github.com>
2021-08-18 08:04:27 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -53,8 +53,8 @@ async def SaveBatchMediaInChannel(bot: Client, editable: Message, message_ids: l
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/JAsuran123"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/JAsuran123")]]
+                 [InlineKeyboardButton("Join Channel", url="https://t.me/ithunammacinema"),
+                  InlineKeyboardButton("Support", url="https://t.me/gpl_kavinbot")]]
             ),
             disable_web_page_preview=True
         )
@@ -86,15 +86,15 @@ async def SaveMediaInChannel(bot: Client, editable: Message, message: Message):
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             parse_mode="Markdown", disable_web_page_preview=True)
-        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(file_er_id)}"
+        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=GPLBlaster_{str_to_b64(file_er_id)}"
         await editable.edit(
             f"**Video Link:\n\n{share_link}**\n\n"
             f"**Just Click the link to get your Videos**. **",
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/JAsuran123"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/JAsuran123")]]
+                 [InlineKeyboardButton("Join Channel", url="https://t.me/ithunammacinema"),
+                  InlineKeyboardButton("Support", url="https://t.me/gpl_kavinbot")]]
             ),
             disable_web_page_preview=True
         )
